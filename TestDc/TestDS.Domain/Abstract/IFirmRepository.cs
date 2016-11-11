c2c48e49f9b8749d23a96a9727915f80a98d1f4b
zavialovserge <zavialovserge@gmail.com>
2016-11-11 23:40:05 +0200

    FirstCommit

---
+++ TestDc/TestDS.Domain/Abstract/IFirmRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDS.Domain.Enteties;
+
+namespace TestDS.Domain.Abstract
+{
+    public interface IFirmRepository
+    {
+        IEnumerable<Firm> Firms { get; }
+    }
+}
---
+++ TestDc/TestDS.Domain/Enteties/Firm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDS.Domain.Enteties
+{
+    public class Firm
+    {
+        public int ID { get; set; } 
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+    }
+}
